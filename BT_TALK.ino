5cc605e34d3424e42b19aec95f7815507416ca3e
Satish <satish.sadashiv@yahoo.in>
2018-09-19 16:59:50 +0530

    Add files via upload

---
+++ BT_TALK.ino
@@ -0,0 +1,33 @@
+// Basic Bluetooth sketch BT_TALK
+// Connect the module and communicate using the serial monitor
+// Communicate with the BT module at 9600 (comms mode)
+ 
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3); // RX | TX
+// Connect the HC-05 TX to Arduino pin 2 RX. 
+// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+// 
+ 
+char c = ' ';
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Arduino is ready");
+    Serial.println("Remember to select Both NL & CR in the serial monitor");
+ 
+    // HC-05 default serial speed for Command mode is 9600
+    BTserial.begin(9600);  
+}
+ 
+void loop()
+{
+ 
+    // Keep reading from HC-05 and send to Arduino Serial Monitor
+    if (BTserial.available())
+    {  
+        c = BTserial.read();
+        Serial.write(c);
+    }
+ 
+}
